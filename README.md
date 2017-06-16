9f2d6085fbf0ec3c17545fad104b83a0f6b70081
Joshua Harms <joshua@nozzlegear.com>
2017-06-16 11:24:58 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # material-ui-datepickerdialog-example
-This is an example of opening the Material-UI DatePicker with a button rather than a text input.
+
+This is an example for opening the Material-UI DatePicker with a button rather than a text input. I created this quick example to document all of the trouble I went through trying to figure out how to do this. 
+
+[You can read more about what needs to be done to fix this bug right here](https://nozzlegear.com/blog/datepickerdialog-how-to-open-material-ui-s-datepicker-with-a-button-or-any-other-element).
+
+## Run the example
+
+Install modules and build the package:
+
+```bash
+# Install dependencies
+yarn install; 
+
+# Build files
+yarn build;
+```
+
+Then open the index.html file in your browser.
---
+++ index.tsx
@@ -0,0 +1,82 @@
+import * as Dom from 'react-dom';
+import * as React from 'react';
+import DatePickerDialog from 'material-ui/DatePicker/DatePickerDialog';
+import { MuiThemeProvider } from 'material-ui/styles';
+
+// Material-UI needs the react-tap-event-plugin activated
+declare const require: any;
+require("react-tap-event-plugin")();
+
+export class MyComponent extends React.Component<any, any> {
+    public state = {
+        selectedDate: new Date()
+    }
+
+    private datePickerDialog: DatePickerDialog;
+
+    private setDatePickerDialogReference(ref: any) {
+        // React passes undefined/null if the reference has been unmounted.
+        if (ref) {
+            // Overwrite the dialog's handleRequestClose and handleWindowKeyUp functions. 
+            ref.handleWindowKeyUp = (...args: any[]) => console.log("Dialog tried to call handleWindowKeyUp.");
+            ref.handleRequestClose = (...args: any[]) => console.log("Dialog tried to call handleRequestClose.");
+        }
+
+        this.datePickerDialog = ref;
+    }
+
+    private openDatePicker() {
+        this.datePickerDialog.show();
+    }
+
+    private openDatePickerOnEnter(e: React.KeyboardEvent<any>) {
+        if (e.key === "Enter") {
+            this.openDatePicker();
+        }
+    }
+
+    private setDateReceived(date: Date) {
+        console.log("Set date received.");
+
+        this.setState({ selectedDate: date });
+    }
+
+    public render() {
+        return (
+            <div>
+                <h1>{`Date Selected: ${this.state.selectedDate.toLocaleDateString()}`}</h1>
+                <ol>
+                    <li>{`Select the text field.`}</li>
+                    <li>{`Press tab to select the button next.`}</li>
+                    <li>{`Press enter with the button selected to open the DatePickerDialog.`}</li>
+                </ol>
+                <input
+                    tabIndex={0}
+                    type={"text"}
+                    style={{ width: 300, margin: "10px, 0" }}
+                    value={"Select me, then press tab and enter."}
+                    readOnly />
+                <div tabIndex={1}>
+                    <button
+                        type={"button"}
+                        onKeyUp={e => this.openDatePickerOnEnter(e)}
+                        onClick={e => this.openDatePicker()}>
+                        {"Open DatePicker"}
+                    </button>
+                </div>
+                <MuiThemeProvider>
+                    <DatePickerDialog
+                        ref={r => this.setDatePickerDialogReference(r)}
+                        firstDayOfWeek={0 /* Must provide firstDayOfWeek or rendering of calendar will be broken. */}
+                        autoOk={false /* Don't close the dialog until the user presses 'Okay'. */}
+                        onAccept={date => this.setDateReceived(date)}
+                        initialDate={this.state.selectedDate} />
+                </MuiThemeProvider>
+            </div>
+        )
+    }
+}
+
+(function () {
+    Dom.render(<MyComponent />, document.getElementById("contenthost"));
+}());
